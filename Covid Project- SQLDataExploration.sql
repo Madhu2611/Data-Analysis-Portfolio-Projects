a393d1602c9e55eb88ac5b937737d20995c771f9
Madhu2611 <45477914+Madhu2611@users.noreply.github.com>
2022-07-21 14:49:20 +0530

    Add files via upload

---
+++ Covid Project- SQLDataExploration.sql
@@ -0,0 +1,28 @@
+
+
+--1. Global Numbers
+select sum(new_cases) as TotalCases, sum(cast(new_deaths as int)) as TotalDeaths , (sum(cast(new_deaths as int))/sum(new_cases))*100 as DeathPercentage from CovidDeathAnalysis..CovidDeaths$
+where continent is not null
+order by 1,2
+
+--2.
+select continent, sum(cast(new_deaths as int)) as TotalDeathCount from CovidDeathAnalysis..CovidDeaths$
+where continent is not null
+and continent not in ('World', 'European Union', 'International')
+Group by continent
+order by TotalDeathCount desc
+
+--3.
+Select continent, location, Population, MAX(total_cases) as HighestInfectionCount,  Max((total_cases/population))*100 as PercentPopulationInfected
+From CovidDeathAnalysis..CovidDeaths$
+--Where location like '%states%'
+Group by continent,location, Population
+order by PercentPopulationInfected desc
+
+
+--4.
+Select location, Population,date, MAX(total_cases) as HighestInfectionCount,  Max((total_cases/population))*100 as PercentPopulationInfected
+From CovidDeathAnalysis..CovidDeaths$
+--Where location like '%states%'
+Group by location,Population, date
+order by PercentPopulationInfected desc
